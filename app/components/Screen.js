22101fd1aa7cc2d8558ad4dccf49f14731f90ffe
Shivam <shivamguptanitw@gmail.com>
2023-07-27 17:24:13 +0530

    Built a simple calculator using reactjs with framework nextjs.

---
+++ app/components/Screen.js
@@ -0,0 +1,16 @@
+'use client'
+import React, { useContext } from 'react'
+import { CalcContext } from '../context/CalContext';
+import { Textfit } from 'react-textfit';
+
+
+const Screen = () => {
+    const { calc } = useContext(CalcContext);
+  return (
+    <Textfit className='screen' max={70} mode='single'>
+        {calc.num ? calc.num : calc.res}
+    </Textfit>
+  )
+}
+
+export default Screen
